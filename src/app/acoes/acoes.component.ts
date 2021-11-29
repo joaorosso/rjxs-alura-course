ed71bfea9cf400306ee75bd7adf9c637981f1e86
Joao Rosso <joaorosso92@gmail.com>
2021-11-29 17:51:15 -0300

    filter and debounceTime

--- src/app/acoes/acoes.component.ts
+++ src/app/acoes/acoes.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { FormControl } from '@angular/forms';
-import { startWith, switchMap } from 'rxjs/operators';
+import { debounceTime, filter, startWith, switchMap } from 'rxjs/operators';
 import { AcoesService } from './acoes.service';
 
 @Component({
@@ -12,6 +12,8 @@ export class AcoesComponent {
   acoesInput = new FormControl();
   actions$ = this.acoesInput.valueChanges.pipe(
     startWith(''),
+    filter(search => search.length >= 3 || !search.length),
+    debounceTime(300),
     switchMap(search => this.acoesService.getActions(search))
   );
 
